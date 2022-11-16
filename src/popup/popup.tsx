95b061e9ebde0dddac2c58c2a20cae97a31a16b6
Christopher Steigerwald <cjsteigerwald@gmail.com>
2022-11-16 13:53:36 -0800

    Adding retrieving seesionStorage

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -3,95 +3,76 @@ import { createRoot } from 'react-dom/client';
 import { Box, Button, Card, CardContent, Stack, Typography } from '@mui/material';
 import ServerCard from '../components/ServerCard/ServerCard';
 import SessionStorageCard from '../components/AddSessionStorageCard/AddSessionStorageCard';
-import { ServerType, getStoredServers, setStoredServers, SessionStorage, getStoredSessionStorage, testTabs, Messages } from '../utils/storage';
+import { ServerType, getStoredServers, setStoredServers, SessionStorage, getStoredSessionStorage,  Messages, deleteServerTypes, returnSessionStorage } from '../utils/storage';
 
 import './popup.css'
 
 const App: React.FC<{}> = () => {
-const [serverType, setServerType] = useState<ServerType[]>([])
-const [sessionStorage, setSessionStorage] = useState<SessionStorage[]>([])
-const [isLoaded, setIsLoaded] = useState<boolean>(false)
+  const [serverType, setServerType] = useState<ServerType[]>([])
+  const [sessionStorage, setSessionStorage] = useState<SessionStorage[]>([])
+  const [isLoaded, setIsLoaded] = useState<boolean>(false)
 
-useEffect(() => {
-  getStoredServers()
-    .then(storedServers => {
-      // initilize serverType if does not already exist in storage
-      if (storedServers.length < 1) {
-        const initialServerValues: ServerType[] = [
-          {
-            name: 'target',
-            url: ''
-          },
-          {
-            name: 'local dev',
-            url: ''
-          }
-        ]
-        setStoredServers(initialServerValues)
-          .then(() => {
-            setServerType(initialServerValues);
-            setIsLoaded(prevState => !prevState)
-          })
-      } else {
-        setServerType(storedServers);
-        setIsLoaded(prevState => !prevState)
+  useEffect(() => {
+    getStoredServers()
+      .then(storedServers => {
+        // initilize serverType if does not already exist in storage
+        if (storedServers.length < 1) {
+          const initialServerValues: ServerType[] = [
+            {
+              name: 'target',
+              url: ''
+            },
+            {
+              name: 'local dev',
+              url: ''
+            }
+          ]
+          setStoredServers(initialServerValues)
+            .then(() => {
+              setServerType(initialServerValues);
+              setIsLoaded(prevState => !prevState)
+            })
+        } else {
+          setServerType(storedServers);
+          setIsLoaded(prevState => !prevState)
+        }
+      })
+
+    getStoredSessionStorage()
+      .then(storedSessionStorage => {
+        setSessionStorage(storedSessionStorage);
+      })
+
+  }, [])
+
+  const onUpdateUrl = (updatedServer: ServerType):void => {
+    const tempServers = serverType.map((server: ServerType) => {
+      if (server.name === updatedServer.name) {
+        return {        
+          ...updatedServer,
+        };
       }
+      return server;   
     })
-
-  getStoredSessionStorage()
-    .then(storedSessionStorage => {
-      setSessionStorage(storedSessionStorage);
+    setStoredServers(tempServers)
+    .then(() => {
+      setServerType(tempServers);
     })
+  } // onUpdateUrl
 
-}, [])
-
-const onUpdateUrl = (updatedServer: ServerType):void => {
-  const tempServers = serverType.map((server: ServerType) => {
-    if (server.name === updatedServer.name) {
-      return {        
-        ...updatedServer,
-      };
-    }
-    return server;   
-  })
-  setStoredServers(tempServers)
-  .then(() => {
-    setServerType(tempServers);
-  })
-} // onUpdateUrl
-
-const handleLoadButton = () => {
-  console.log('Testing onUpdateUrl', onUpdateUrl)
-  setIsLoaded(prevState => !prevState)
-}
-
-const handleClearButton = () => {
-  console.log('Clear Button')
-}
-
-// const sessionStore: SessionStorage[] = [{ name: 'header_1', value: 'header_value_1'}, { name: 'header_2', value: 'header_value_2'}];
-const sessionStore: SessionStorage[] = [
-  {
-      "name": "_c;;i",
-      "value": "p:*|l:9007199254740991_31"
-  },
-  {
-      "name": "sr;;1668539950468",
-      "value": "p:m|l:29_{\"current\":{\"Wya\":{\"d\":645},\"RBb\":1668539950467},\"last\":{\"Wya\":{},\"RBb\":-1}}"
-  },
-  {
-      "name": "hsb;;1668539950468",
-      "value": "p:*|l:1_{\"state\":null,\"url\":\"/search?q=sessionStorage&oq=sessionStorage&aqs=chrome..69i57j0i20i263i512j0i512l3j69i60l3.10356j0j7&sourceid=chrome&ie=UTF-8\",\"metadata\":{\"nRa\":1668539950467,\"Il\":1668539950468,\"Zja\":1668539950469,\"LG\":0}}"
-  },
-  {
-      "name": "hsb;;1668539950469",
-      "value": "p:*|l:0_[1668539950468]"
-  },
-  {
-      "name": "eob;;xplsd",
-      "value": "p:m|l:30_{\"sessionStorage\":[{\"id\":\"eob11\",\"timestamp\":1668540193333}]}"
+  let sessionStore: SessionStorage[] = []
+  const handleLoadButton = () => {
+    console.log('Testing onUpdateUrl', onUpdateUrl)
+    setIsLoaded(prevState => !prevState)
+    returnSessionStorage().then(resp => {
+      setSessionStorage(resp);
+    })
   }
-];
+
+  const handleClearButton = () => {
+    const test = deleteServerTypes()    
+    console.log('Clear Button')
+  }
 
   return (
    <Box mx='8px' my='16px'>
@@ -122,11 +103,12 @@ const sessionStore: SessionStorage[] = [
           </Button>        
         </Box>
       </CardContent>      
-    </Card>    
-    <SessionStorageCard sessionStorage={sessionStore} />
+    </Card>
+    {
+      sessionStorage ? <SessionStorageCard sessionStorage={sessionStorage} /> : null
+    }    
    </Box>
   )
-
 } // APP
 
 
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,3 +1,4 @@
+import { resolveComponentProps } from '@mui/base';
 export interface ChromeStorage {
   serverType?: ServerType[];
   sessionStorage?: SessionStorage[];
@@ -47,6 +48,7 @@ export const getStoredServers = (): Promise<ServerType []> => {
   const keys: LocalStorageKeys[] = ['serverType'];
   return new Promise((resolve) => {
     chrome.storage.local.get(keys, (res: ChromeStorage) => {
+      console.log('getStoredServers: ', res.serverType)
       resolve(res.serverType ?? []);
     })
   })
@@ -72,55 +74,52 @@ export const getStoredSessionStorage = (): Promise<SessionStorage []> => {
   })
 }
 
-const getSessionStorage = () => {
+// helper function for returnSessionStorage()
+const getSessionStorage = (): SessionStorage[] => {
   let aSessionStorage: SessionStorage[] = [];
 
   Object.keys(sessionStorage).forEach((key) => {
     console.log('', [key] + ':' + '', sessionStorage.getItem(key))
     aSessionStorage.push({name: String([key]), value: sessionStorage.getItem(key)})
-    // aSessionStorage[key] = sessionStorage.getItem(key);
   })
-
-  console.log('In get title', aSessionStorage)
-
   return aSessionStorage;
 }
 
-export const testTabs = () => {
-  
-  chrome.tabs.query({
-    url: "*://www.google.com/*",
-    currentWindow: true,
-  }, (tabs) => {
-    if (tabs.length > 0) {
-      const id = tabs[0].id
-      console.log('tab id: ', tabs[0]);
-      // chrome.tabs.sendMessage(tabs[0].id, Messages.GET_SESSION_STORAGE, (response) => {
-      //   console.log('Storage: In response ')
-      // })
-      chrome.scripting.executeScript({
-        target: {tabId: id},
-        func: getSessionStorage,
-      },(resp) => {
-        console.log('This is response: ', JSON.stringify(resp[0].result))
-        console.log('This is tab: ', tabs[0])
-        console.log('below script: ', sessionStorage.length)
+export const returnSessionStorage = (): Promise<SessionStorage []> => {
+  return new Promise<SessionStorage []>((resolve) => {
+
+      let aSesssionStorage: SessionStorage[] = []
+      chrome.tabs.query({
+        url: "*://www.google.com/*",
+        currentWindow: true,
+      }, (tabs) => {
+        if (tabs.length > 0) {
+          const id = tabs[0].id
+          console.log('tab id: ', tabs[0]);
+          chrome.scripting.executeScript({
+            target: {tabId: id},
+            func: getSessionStorage,
+          },(resp) => {
+            console.log('This is response: ', JSON.stringify(resp[0].result))
+            // console.log('This is tab: ', tabs[0])
+            // console.log('below script: ', sessionStorage.length)
+            aSesssionStorage = resp[0].result
+            console.log('aSessionStorage: ', aSesssionStorage)
+            resolve (resp[0].result)
+          })
+        } 
       })
-      // chrome.tabs.sendMessage(tabs[0].id, Messages.GET_SESSION_STORAGE)
-    } else {
-      console.log('tab not found')
-    }
   })
   
 }
 
-// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//   console.log(message);
-//   console.log(sender);
-//   sendResponse('From the storage listener script');
-// })
+export const deleteServerTypes = () => {
+  const keys: LocalStorageKeys[] = ['serverType'];
+  chrome.storage.local.remove(keys, () => {
+    console.log('Removed items for the key: ' + keys);
+  });
 
-
+}
 
 
 
